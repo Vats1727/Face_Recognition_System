39f73f22ebb6cb1005f0c994f728c78da0a520c3
Vats1727 <120392290+Vats1727@users.noreply.github.com>
2022-12-13 18:10:38 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,87 +1,47 @@
 import cv2
 import numpy as np
-# noinspection PyUnresolvedReferences
-import webbrowser
-from os import listdir
-from os.path import isfile, join
-
-data_path = 'C:/Users/Vatsal/Downloads/faces/'
-onlyfiles = [f for f in listdir(data_path) if isfile(join(data_path,f))]
-
-Training_Data, Labels = [], []
-
-for i, files in enumerate(onlyfiles):
-    image_path = data_path + onlyfiles[i]
-    images = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-    Training_Data.append(np.asarray(images, dtype=np.uint8))
-    Labels.append(i)
-
-Labels = np.asarray(Labels, dtype=np.int32)
-
-model = cv2.face.LBPHFaceRecognizer_create()
-
-model.train(np.asarray(Training_Data), np.asarray(Labels))
-
-print("Model Training Complete!!!!!")
 
 face_classifier = cv2.CascadeClassifier('C:/python3.10/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml')
 
-def face_detector(img, size = 0.5):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def face_extractor(img):
+
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_classifier.detectMultiScale(gray,1.3,5)
 
-    if  faces == ():
-        return img,[]
+    if faces is ():
+        return None
 
     for(x,y,w,h) in faces:
-        cv2.rectangle(img, (x,y),(x+w,y+h),(0,255,255),2)
-        roi = img[y:y+h, x:x+w]
-        roi = cv2.resize(roi, (200,200))
+        cropped_face = img[y:y+h, x:x+w]
 
-    return img,roi
+    return cropped_face
+
 
 cap = cv2.VideoCapture(0)
+count = 0
+
 while True:
-
-    ret, im = cap.read()
-
-    image, face = face_detector(im)
-
-
-    try:
+    ret, frame = cap.read()
+    if face_extractor(frame) is not None:
+        count+=1
+        face = cv2.resize(face_extractor(frame),(200,200))
         face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
-        result = model.predict(face)
 
-        if result[1] < 500:
-            confidence = int(100*(1-(result[1])/300))
-            display_string = str(confidence)+'% Confidence it is  not user'
-            cv2.putText(image, display_string, (100, 120), cv2.FONT_HERSHEY_COMPLEX, 1, (250, 120, 255), 2)
+        file_name_path = 'C:/Users/Vatsal/Downloads/faces/sample'+str(count)+'.jpg'
+        cv2.imwrite(file_name_path,face)
 
-
-
-        if (confidence >= 85):
-
-            cv2.putText(image, "Unlocked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
-
-            webbrowser.open("https://chatapp24by7.netlify.app/")
-            cv2.imshow('Face Cropper', image)
-            break
-
-        else:
-            cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-            cv2.imshow('Face Cropper', image)
-
-
-
-    except:
-        cv2.putText(image, "Face Not Found", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2)
-        cv2.putText(image, "Locked", (250, 450), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
-        cv2.imshow('Face Cropper', image)
+        cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+        cv2.imshow('Face Cropper',face)
+    else:
+        print("Face  Found")
         pass
 
-    if cv2.waitKey(1)==13:
-        break
+    if cv2.waitKey(1)==13 or count==100:
 
+
+        break
 
 cap.release()
 cv2.destroyAllWindows()
+print('Colleting Samples Complete!!!')
